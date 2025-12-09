c30011841d47382bdebad1b9df25549d83af99b0
sereneerine <sereneerine@gmail.com>
2025-12-09 13:16:50 +0500

    Create VariantDrawer.tsx

---
+++ components/VariantDrawer.tsx
@@ -0,0 +1,8 @@
+export default function VariantDrawer() {
+  return (
+    <div>
+      <h4 className="font-semibold">Variant Detail</h4>
+      <div className="text-silver text-sm mt-2">Select a variant from the table to see details here.</div>
+    </div>
+  );
+}
